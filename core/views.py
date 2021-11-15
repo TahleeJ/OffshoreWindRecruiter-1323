40e1678cf5a5868410b6ea8e67df1f91ae14db46
tylcos <tylcos@users.noreply.github.com>
2021-11-15 14:39:02 -0500

    Add API test view

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 from core.models import Question
 
@@ -12,5 +13,10 @@ def index(request):
     return render(request, 'survey/index.html', context)
 
 
-def static_test(request):
+def static_test(request):   
     return render(request, 'static_test.html')
+    
+
+def api_test(request):
+    data = list(Question.objects.all().values())
+    return JsonResponse(data, safe=False)

2b4427ac9fb96dc5a9b2ab1515124c48bcf02f0b
sduong31 <73000871+sandytduong@users.noreply.github.com>
2022-01-17 16:33:55 -0500

    Edited Admin Home page

--- src/react components/AdminHome.tsx
+++ src/react components/AdminHome.tsx
@@ -9,7 +9,21 @@ interface props {
 const AdminHome: React.FC<props> = (props) => {
 
     return (
-        <div>THIS ELEMENT NEEDS TO BE DEVELOPED</div>
+        <div id="adminHome" className='adminContainer'>
+            <div className="userInfo">
+                <p>[First Name] [Last Name] <br/>
+                Email <br/>
+                Administrator
+                </p>
+            </div>
+            <h1 id="adminPrompt">What would you like to do?</h1>
+            <div className='adminButtons'>
+                <button className='adminButton'>Manage Job Opportunities</button>
+                <button className='adminButton'>Manage Surveys</button>
+                <button className='adminButton'>Manage Labels</button>
+                <button className='adminButton'>Promote User</button>
+            </div>
+        </div>
     );
 }
 

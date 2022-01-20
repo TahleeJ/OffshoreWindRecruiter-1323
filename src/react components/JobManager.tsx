79bd82bf595013260d171aae566739a967895f18
xwz723 <69948121+xwz723@users.noreply.github.com>
2022-01-20 11:09:01 -0500

    Create Job opportunities page

--- src/react components/JobManager.tsx
+++ src/react components/JobManager.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ListViewer from './ListViewer';
 
 /** The props (arguments) to create this element */
 interface props {
@@ -7,9 +8,36 @@ interface props {
 
 /** The header of the application. */
 const JobManager: React.FC<props> = (props) => {
-
     return (
-        <div>THIS ELEMENT NEEDS TO BE DEVELOPED</div>
+        <div id = 'jobManager' className = "jobManage">
+            <div className = 'jobHeader'>
+                <div className='jobTitle'>
+                    Opportunities
+                </div>
+                <div className='jobButton'>
+                    <button onClick={() => alert("Not done")}>New Opportunity</button>
+                </div>
+            </div>
+            <div className = 'searchBox'>
+                <div className='searchText'>
+                    Search: 
+                </div>
+                <div className='searchBar'>
+                    <input placeholder="Search Opportunity"></input>
+                </div>
+            </div>
+            <ListViewer height="calc(100% - 500px)" title='Opportunities'>
+                <div>TEST OPPORTUNITY</div>
+                <div>TEST OPPORTUNITY</div>
+                <div>TEST OPPORTYNITY</div>
+                <div>TEST OPPORTYNITY</div>
+                <div>TEST OPPORTYNITY</div>
+                <div>TEST OPPORTYNITY</div>
+                <div>TEST OPPORTYNITY</div>
+                <div>TEST OPPORTYNITY</div>
+                <div>TEST OPPORTYNITY</div>
+            </ListViewer>
+        </div>
     );
 }
 

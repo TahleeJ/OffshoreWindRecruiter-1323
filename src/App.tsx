9373350eb00dfc627ec1ce23d017c2c0da14fa67
TahleeJ <tahlee.jaynes@gmail.com>
2022-02-10 21:13:43 -0500

    Add initial Firebase auth UI, checkAdmin function, and Firestore retrieval

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React, { createContext } from 'react';
 import './styling/App.css';
-
-import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports';
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+import { initializeApp } from "firebase/app";
+import * as firebaseui from 'firebaseui';
+import * as firebaseAuth from "@firebase/auth";
+import * as firestore from "@firebase/firestore";
+import * as functions from "@firebase/functions";
 
 import Home from './react components/Home'
 import { useAppSelector } from './redux/hooks';
@@ -14,37 +14,21 @@ import Header from './react components/Header';
 import SurveyHome from './react components/survey/SurveyHome';
 import AdminManager from './react components/AdminManager';
 import LabelManager from './react components/LabelManager';
-
 import JobManager from './react components/Job/JobManager';
 
+// Your web app's Firebase configuration
+require('dotenv').config();
 
-// Multiple redirect URI handling for OAuth 
-const isLocalhost = Boolean(
-    window.location.hostname === "localhost" ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === "[::1]" ||
-    // 127.0.0.1/8 is considered localhost for IPv4.
-    window.location.hostname.match(
-        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
-);
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+};
 
-// Assuming you have two redirect URIs, and the first is for localhost and second is for production
-const [
-    localRedirectSignIn,
-    productionRedirectSignIn,
-] = awsconfig.oauth.redirectSignIn.split(",");
-
-const [
-    localRedirectSignOut,
-    productionRedirectSignOut,
-] = awsconfig.oauth.redirectSignOut.split(",");
-
-awsconfig.oauth.redirectSignIn = isLocalhost ? localRedirectSignIn : productionRedirectSignIn;
-awsconfig.oauth.redirectSignOut = isLocalhost ? localRedirectSignOut : productionRedirectSignOut;
-
-Amplify.configure(awsconfig);
-
+export const firebaseApp = initializeApp(firebaseConfig);
 export const AuthContext = createContext({} as any);
 
 const getOverallPageFromType = (type: PageType) => {
@@ -54,24 +38,72 @@ const getOverallPageFromType = (type: PageType) => {
         case PageType.Survey: return <SurveyHome />
         case PageType.AdminManage: return <AdminManager />
         case PageType.LabelManage: return <LabelManager />
-
         case PageType.JobManage: return <JobManager />
     }
 }
 
-const App: React.FC = () => {
-    const pageType = useAppSelector(s => s.navigation.currentPage);
+const authInstance = firebaseAuth.getAuth(firebaseApp);
+const firestoreInstance = firestore.getFirestore(firebaseApp);
+const functionsInstance = functions.getFunctions(firebaseApp);
 
-    return (
-        <Authenticator socialProviders={['google']} variation="modal" loginMechanisms={['email']}>
-            {({ signOut, user }) => (
-                <AuthContext.Provider value={{ user, signOut }}>
-                    <Header />
-                    {//we show the page depending on which PageType is currently in our redux state
-                        getOverallPageFromType(pageType)}
-                </AuthContext.Provider>
-            )}
-        </Authenticator>
+const checkAdmin = functions.httpsCallable(functionsInstance, 'checkAdmin');
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(authInstance);
+
+const App: React.FC = () => {
+    authInstance.onAuthStateChanged(async (user) => {
+        if (user) {
+            alert("Good auth state");
+            firestore.setDoc(
+                firestore.doc(firestoreInstance, "User", `${user.uid}`), 
+                {
+                    email : user.email, 
+                    isAdmin: true, 
+                    dummyData: "dummmmmy"
+                }
+            );
+
+            await checkAdmin({})
+                .then((result) => {
+                    const usableData: Object = result.data as Object;
+                    const dataMap = new Map(Object.entries(usableData));
+                    console.log(dataMap.get("isAdmin"));
+                    console.log(dataMap.get("text"));
+                })
+        } else {
+            alert("Bad auth state");
+        }
+    });
+
+    console.log("firebase");
+    const pageType = useAppSelector(s => s.navigation.currentPage);
+    
+    ui.start('#firebaseui-auth-container', {
+        signInOptions: [
+            {
+                provider: firebaseAuth.EmailAuthProvider.PROVIDER_ID,
+                requireDisplayName: false
+            },
+            {
+                provider: firebaseAuth.GoogleAuthProvider.PROVIDER_ID,
+                customParameters: {
+                    // Forces account selection even when one account
+                    // is available.
+                    prompt: 'select_account'
+                }
+            }
+        ], 
+    });
+
+    firebaseAuth.setPersistence(authInstance, firebaseAuth.browserLocalPersistence);
+
+    // getOverallPageFromType(pageType);
+
+    return(
+        <><Header /><div id="firebaseui-auth-container">
+
+        </div></>
     );
 }
 
--- src/react components/Home.tsx
+++ src/react components/Home.tsx
@@ -3,22 +3,28 @@ import { AuthContext } from '../App';
 import { useAppDispatch } from '../redux/hooks';
 import { changePage, OperationType, PageType } from '../redux/navigationSlice';
 import ListViewer from './ListViewer';
+import { firebaseApp } from "../App";
+import * as firebaseAuth from "@firebase/auth";
 
 interface props {
 
 }
 
+// var firebase = require('firebase');
+
 const Home: React.FC<props> = (props) => {
     const appDispatch = useAppDispatch();
-    const auth = useContext(AuthContext);
-    const user = auth.user;
-    console.log(user); //this is to figure out what is all on the user type
-
+    // const auth = useContext(AuthContext);
+    const authInstance = firebaseAuth.getAuth(firebaseApp);
+    const user = authInstance.currentUser;
+    // console.log(user); //this is to figure out what is all on the user type
+    
 
     return (
         <div id="home">
             <h1 id="account">Account Management</h1>
-            <p id="userEmail">{user.attributes.email}</p>
+            {/* <p id="userEmail">{user.attributes.email}</p> */}
+            <p id="userEmail">{user?.email}</p>
             <ListViewer height="calc(100% - 300px)" title='Survey Templates'>
                 <div onClick={() => appDispatch(changePage({ type: PageType.Survey, operation: OperationType.Administering }))}>TEST ITEM (this will be a survey template eventually)</div>
                 <div onClick={() => appDispatch(changePage({ type: PageType.Survey, operation: OperationType.Administering }))}>TEST ITEM (this will be a survey template eventually)</div>

83c9b6b33fc05847c32a7d9312c9235a09de3b7b
SeanKaat <seankaat@gmail.com>
2022-02-13 21:02:08 -0500

    auth uses email and password now

--- src/react components/AuthPage.tsx
+++ src/react components/AuthPage.tsx
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import * as firebaseAuth from "@firebase/auth";
 import { firebaseApp } from '../App';
 import { FirebaseError } from 'firebase/app';
+// import googleAuthIcon from '../icons/google-authentication-management.png';
 
 const AuthPage = () => {
     const [error, setError] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
     const authInstance = firebaseAuth.getAuth(firebaseApp);
     const gProvider = new firebaseAuth.GoogleAuthProvider();
 
+
     const createAccount = async () => {
         try {
-            const result = await firebaseAuth.createUserWithEmailAndPassword(authInstance, "TEST@test.com", "testPassword");
+            const result = await firebaseAuth.createUserWithEmailAndPassword(authInstance, username, password);
             console.log(result)
         } catch (e) {
             setError("Error creating account: " + (e as FirebaseError).code)
@@ -18,7 +22,7 @@ const AuthPage = () => {
     }
     const signIn = async () => {
         try {
-            await firebaseAuth.signInWithEmailAndPassword(authInstance, "TEST@test.com", "testPassword");
+            await firebaseAuth.signInWithEmailAndPassword(authInstance, username, password);
         } catch (e) {
             setError("Error signing into account: " + (e as FirebaseError).code)
         }
@@ -36,12 +40,11 @@ const AuthPage = () => {
             {error ? <div className='error'>{error}</div> : null}
             <div className='content'>
                 <div className='title'>Sign-In/Create Account</div>
-                <input type="text" id="email" placeholder='Email' /><br />
-                <input type="text" id="password" placeholder='Password' /><br />
                 <button onClick={googleSignIn}>Use Google</button>
-                <br />
+                <input type="text" id="email" placeholder='Email' value={username} autoComplete="on" onChange={(e) => setUsername(e.target.value)}/><br />
+                <input type="password" id="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)}/><br />
                 <button onClick={createAccount}>Create Account</button>
-                <br/>
+                <br />
                 <button onClick={signIn}>Sign-In</button>
             </div>
         </div>

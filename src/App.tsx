d4f1ecd6451502cf9de61f5ff82bdb97acd33fd2
TahleeJ <tahlee.jaynes@gmail.com>
2022-02-14 14:03:52 -0500

    Removed personal email

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,7 @@ const App: React.FC = () => {
             const updateAdmin = functions.httpsCallable(functionsInstance, 'updateAdmin');
 
             try {
-                await updateAdmin({userEmail: "tyler.cosentino1@gmail.com", newPermissionLevel: PermissionLevel.Owner});
+                await updateAdmin({userEmail: <insert email here>, newPermissionLevel: PermissionLevel.Owner});
 
                 console.log("Update success!");
             } catch (error) {

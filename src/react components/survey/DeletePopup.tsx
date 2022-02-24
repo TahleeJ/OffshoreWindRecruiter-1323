66685d54212249d6079ba256eced879f3a4b7495
xwz723nn <79524956+xwz723nn@users.noreply.github.com>
2022-02-24 00:07:05 -0500

    Survey delete popup created

---
+++ src/react components/survey/DeletePopup.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+/** The props (arguments) to create this element */
+interface props {
+    handleCancel: () => void;
+    handleDelete: () => void;
+}
+
+/** The header of the application. */
+const DeletePopup: React.FC<props> = (props) => {
+    return  (
+        <div id='Popup' className='Popup'>
+            <div className = 'popBox'>
+                <div className='PopupText'>
+                    <h1>Are you sure you want to delete this opportunity?</h1>
+                    <p>This action is permanent</p>
+                </div>
+                <div className='Buttons'>
+                    <button className="red" onClick={props.handleDelete}>Delete</button>
+                    <button onClick={props.handleCancel}>Cancel</button>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default DeletePopup
+
--- src/react components/survey/ListElement.tsx
+++ src/react components/survey/ListElement.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import DeletePopup from "./DeletePopup";
+
+
 
 interface props {
     name: string;
@@ -7,12 +10,19 @@ interface props {
 }
 
 const ListElement: React.FC<props> = (p) => {
-
+    const [popupVisible, setPopupvisible] = useState<Boolean>(false);
+    const togglePopup = () => {
+        setPopupvisible(!popupVisible);
+    }
     return (
         <div className="listElement">
             <div className="name">{p.name}</div>
             <button className="edit" onClick={p.handleEdit}>Edit</button>
-            <button className="red" onClick={p.handleDelete}>Delete</button>
+            <button className='red' onClick={togglePopup}>Delete</button>
+            
+            {popupVisible && <DeletePopup handleCancel={togglePopup} handleDelete={p.handleDelete}></DeletePopup>}
+
+            
         </div>
     )
 }

e3ed87e8c9a4e88e399166e6ad320cd4372eee61
TahleeJ <tahlee.jaynes@gmail.com>
2022-03-13 20:06:03 -0400

    Rename test/init.ts

---
+++ functions/test/Init.ts
@@ -0,0 +1,27 @@
+/**
+ * **WARNING:**
+ * 
+ * This file is crucial to the set up of the testing environment. 
+ * The ordering of everything in this file is important and should 
+ * NOT be touched or rearranged!
+ */
+
+ import { initTestDocs } from './Utility';
+ import * as functionsTest from 'firebase-functions-test';
+ 
+ /**
+  * Initializes the testing environment with test users and functions
+  * to be emulated
+  * 
+  * @returns the in-development cloud functions
+  */
+ export async function initializeTestEnvironment() {
+     let myFunctions = require('../src/index.js');
+ 
+     await initTestDocs();
+ 
+     return myFunctions;
+ }
+ 
+ export const testEnv = functionsTest();
+ 

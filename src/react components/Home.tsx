75b88c60e672c47266ab07dfe701e4c042d825da
SeanKaat <seankaat@gmail.com>
2022-03-25 00:24:00 -0400

    can view surveys in card form now

--- src/react components/Home.tsx
+++ src/react components/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { authInstance } from "../firebase/Firebase";
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import { changePage, OperationType, PageType } from '../redux/navigationSlice';
@@ -18,16 +18,18 @@ interface props {
 
 const Home: React.FC<props> = (props) => {
     const surveys = useAppSelector(s => s.data.surveys);
+    const responses = useAppSelector(s => s.data.surveyResponses);
+    const [isListView, setListView] = useState(true);
     const appDispatch = useAppDispatch();
     const user = authInstance.currentUser;
 
     return (
         <div id="home">
-            {/* <h1 id="account">Administer Survey:</h1> */}
             <p id="userEmail">{user?.email}</p>
-            <ListViewer height="calc(100% - 100px)" title='Administer Survey'>
-                {
-                    surveys.length > 0 ?
+            <button className="toggle" onClick={() => setListView(!isListView)}>{!isListView ? "List View" : "Card View"}</button>
+            {isListView ?
+                <ListViewer height="calc(100% - 130px)" title='Administer Survey'>
+                    {surveys.length > 0 ?
                         surveys.map((survey, ind) => {
                             return <div
                                 key={ind}
@@ -43,8 +45,32 @@ const Home: React.FC<props> = (props) => {
                             </div>
                         })
                         : <div>There are no survey templates at the moment</div>
-                }
-            </ListViewer>
+                    }
+                </ListViewer>
+                :
+                <div className='cards'>
+                    {surveys.length > 0 ?
+                        surveys.map((survey, ind) => (
+                            <div
+                                key={ind}
+                                className="card"
+                                onClick={() => {
+                                    appDispatch(changePage({
+                                        type: PageType.Survey,
+                                        operation: OperationType.Administering,
+                                        data: survey
+                                    }))
+                                }}>
+                                <div className='title'>{survey.title}</div>
+                                <div className='description'>{survey.description ? survey.description : "No Description"}</div>
+                                <div className='questions'>{survey.questions.length} question(s)</div>
+                                <div className='responses'>{responses.filter(r => r.surveyId == survey.id).length} responses(s)</div>
+                            </div>
+                        ))
+                        : <div>There are no survey templates at the moment</div>
+                    }
+                </div>
+            }
         </div>
     );
 }

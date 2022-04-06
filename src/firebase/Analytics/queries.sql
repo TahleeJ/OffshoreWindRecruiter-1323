e5d97076e81485f53e3ee227b9df17ae2e4aae62
TahleeJ <tahlee.jaynes@gmail.com>
2022-04-05 23:09:43 -0400

    Update queries

--- src/firebase/Analytics/queries.sql
+++ src/firebase/Analytics/queries.sql
@@ -9,6 +9,9 @@
 
     It is highly recommended to edit queries at the link above as that will
     provide the quickest method of testing.
+
+    This particular query file can be found here: 
+    https://console.cloud.google.com/bigquery?sq=533164020983:4a7e31672a174edfa892a313a8a443e5
 */
 
 -- Total survey title per day
@@ -26,7 +29,8 @@ FROM (
             WHERE key = "administered_survey_title") AS survey_title
     FROM `hallowed-digit-338620.analytics_305371849.events_intraday_*`
     WHERE event_name = "survey_administered"))
-GROUP BY event_date, survey_title;
+GROUP BY event_date, survey_title
+ORDER BY event_date DESC;
 
 -- Total survey per day
 CREATE OR REPLACE TABLE FUNCTION analytics_305371849.get_all_day() AS
@@ -43,7 +47,8 @@ FROM (
             WHERE key = "administered_survey_title") AS survey_title
     FROM `hallowed-digit-338620.analytics_305371849.events_intraday_*`
     WHERE event_name = "survey_administered"))
-GROUP BY event_date;
+GROUP BY event_date
+ORDER BY event_date DESC;
 
 -- Total survey title
 CREATE OR REPLACE TABLE FUNCTION analytics_305371849.get_all_titles() AS
@@ -81,7 +86,8 @@ FROM (
             WHERE key = "administered_survey_title") AS survey_title
     FROM `hallowed-digit-338620.analytics_305371849.events_intraday_*`
     WHERE event_name = "survey_administered"))
-GROUP BY event_date, survey_title, navigator;
+GROUP BY event_date, survey_title, navigator
+ORDER BY event_date DESC;
 
 -- Total survey per day for each
 CREATE OR REPLACE TABLE FUNCTION analytics_305371849.get_each_day() AS
@@ -102,7 +108,8 @@ FROM (
             WHERE key = "administered_survey_title") AS survey_title
     FROM `hallowed-digit-338620.analytics_305371849.events_intraday_*`
     WHERE event_name = "survey_administered"))
-GROUP BY event_date, navigator;
+GROUP BY event_date, navigator
+ORDER BY event_date DESC;
 
 -- Total survey for each
 CREATE OR REPLACE TABLE FUNCTION analytics_305371849.get_each_titles() AS
@@ -144,7 +151,8 @@ FROM (
             WHERE key = "administering_navigator") AS navigator
     FROM `hallowed-digit-338620.analytics_305371849.events_intraday_*`, UNNEST(event_params) AS P
     WHERE event_name = "survey_administered" AND P.value.string_value = name))
-GROUP BY event_date, survey_title, navigator;
+GROUP BY event_date, survey_title, navigator
+ORDER BY event_date DESC;
 
 -- Total survey per day for navigator
 CREATE OR REPLACE TABLE FUNCTION analytics_305371849.get_navigator_day(name STRING) AS
@@ -165,7 +173,8 @@ FROM (
             WHERE key = "administering_navigator") AS navigator
     FROM `hallowed-digit-338620.analytics_305371849.events_intraday_*`, UNNEST(event_params) AS P
     WHERE event_name = "survey_administered" AND P.value.string_value = name))
-GROUP BY event_date, navigator;
+GROUP BY event_date, navigator
+ORDER BY event_date DESC;
 
 -- Total survey for navigator
 CREATE OR REPLACE TABLE FUNCTION analytics_305371849.get_navigator_titles(name STRING) AS

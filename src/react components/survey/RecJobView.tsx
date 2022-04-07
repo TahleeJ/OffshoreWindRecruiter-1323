f6c08855d4072d3c2dec1fd6d1e2c45a6568bad0
Sean K <58948696+SeanKaat@users.noreply.github.com>
2022-04-07 00:06:06 -0400

    abstract recommend job view

---
+++ src/react components/survey/RecJobView.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { RecommendedJobs } from '../../firebase/Types';
+import { useAppSelector } from '../../redux/hooks';
+
+interface props {
+
+}
+
+const RecJobView: React.FC<props> = (p) => {
+    const labels = useAppSelector(s => s.data.labels);
+    const recommendedJobs = useAppSelector(s => s.navigation.operationData as RecommendedJobs);
+
+    return (
+        <>
+            <span>Here are some job recommendations that align with your survey answers:</span>
+            {recommendedJobs?.map((recommendation, index) => (
+                <div key={index} className={"recommendation " + ((recommendation.score > 0) ? "positive" : (recommendation.score >= -0.5) ? "neutral" : "negative")}>
+                    <div className='title'>{recommendation.jobOpp.jobName}</div>
+                    <div className=''>{recommendation.jobOpp.companyName}</div>
+                    <div className=''>{recommendation.jobOpp.jobDescription}</div>
+                    <div className=''>{recommendation.jobOpp.jobLink}</div>
+                    <div className=''>
+                        {recommendation.jobOpp.labelIds.map((l, i) => (
+                            <span key={i}>-{labels.find(searchLabel => searchLabel.id === l)?.name}- </span>
+                        ))
+                        }
+                    </div>
+                    {recommendation.score > 0 ?
+                        <span>Based on your answers to the survey, we {recommendation.score >= .5 ? "highly" : ""} recommend this job</span>
+                        :
+                        <span>Based on your answers to the survey, we {recommendation.score <= -.5 ? "highly" : ""} do not recommend this job</span>
+                    }
+                </div>
+            ))}
+        </>
+    )
+}
+
+export default RecJobView;
--- src/react components/survey/SurveyResult.tsx
+++ src/react components/survey/SurveyResult.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 import { RecommendedJobs } from '../../firebase/Types';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { changePage, PageType, Status } from '../../redux/navigationSlice';
+import RecJobView from './RecJobView';
 
 
 const SurveyReviewer: React.FC = _ => {
     const status = useAppSelector(s => s.navigation.status);
-    const labels = useAppSelector(s => s.data.labels);
-    const recommendedJobs = useAppSelector(s => s.navigation.operationData as RecommendedJobs);
+
     const dispatch = useAppDispatch();
 
     return (
@@ -17,30 +17,7 @@ const SurveyReviewer: React.FC = _ => {
             <div className=''>
                 <div className='surveyTitle' >Recommended Jobs</div>
                 <div className=''>
-                    {status === Status.fulfilled &&  // Could also handle a rejected request
-                        <>  
-                            <span>Here are some job recommendations that align with your survey answers:</span>
-                            {recommendedJobs?.map((recommendation, index) => (
-                                <div key={index} className={"recommendation " + ((recommendation.score > 0) ? "positive" : (recommendation.score >= -0.5) ? "neutral" : "negative")}>
-                                    <div className='title'>{recommendation.jobOpp.jobName}</div>
-                                    <div className=''>{recommendation.jobOpp.companyName}</div>
-                                    <div className=''>{recommendation.jobOpp.jobDescription}</div>
-                                    <div className=''>{recommendation.jobOpp.jobLink}</div>
-                                    <div className=''>
-                                        {recommendation.jobOpp.labelIds.map((l, i) => (
-                                            <span key={i}>-{labels.find(searchLabel => searchLabel.id === l)?.name}- </span>
-                                        ))
-                                        }
-                                    </div>
-                                    {recommendation.score > 0 ?
-                                        <span>Based on your answers to the survey, we {recommendation.score >= .5 ? "highly" : ""} recommend this job</span>
-                                        :
-                                        <span>Based on your answers to the survey, we {recommendation.score <= -.5 ? "highly" : ""} do not recommend this job</span>
-                                    }
-                                </div>
-                            ))}
-                        </>
-                    }
+                    {status === Status.fulfilled && <RecJobView />}
                     {status === Status.pending &&
                         <>
                             Loading Results:
@@ -50,7 +27,7 @@ const SurveyReviewer: React.FC = _ => {
                     {status === Status.rejected &&
                         <>
                             The survey submission has been rejected. The recommended jobs can't be displayed at this time.
-                            <br/>
+                            <br />
                             We are sorry for the inconvenience!
                         </>
                     }

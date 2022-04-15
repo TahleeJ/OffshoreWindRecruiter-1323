961957817f00fce0b0ce5c7ec034892ac710324f
Emir Ibrisimovic <emiribrisimovicc@gmail.com>
2022-04-14 21:04:17 -0400

    currently downloads pdf of result screen

--- src/react components/survey/RecJobView.tsx
+++ src/react components/survey/RecJobView.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { RecommendedJobWithData } from '../../firebase/Types';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+import $ from 'jquery';
 
 interface props {
     jobs: RecommendedJobWithData[];
 }
 
 const RecJobView: React.FC<props> = (p) => {
+    var doc = new jsPDF();
     return (
         <>
             {/* <span>Here are some job recommendations that align with your survey answers:</span> */}
+            <div className = "printThis">
             {p.jobs.map((recommendation, index) => (
                 <div id="jobRec" key={index} className={"recommendation " + ((recommendation.score > 0) ? "positive" : (recommendation.score >= -0.5) ? "neutral" : "negative")}>
                     <div className="jobRecContainer">
@@ -38,8 +43,48 @@ const RecJobView: React.FC<props> = (p) => {
                     </div> */}
                 </div>
             ))}
+            <button onClick={() => { 
+                getPDF()
+            }}>Download Results</button>
+            </div>
         </>
     )
 }
 
 export default RecJobView;
+
+  function getPDF(){
+
+    var HTML_Width = $(".printThis").width()!;
+    var HTML_Height = $(".printThis").height()!;
+    var top_left_margin = 15;
+    var PDF_Width = HTML_Width+(top_left_margin*2);
+    var PDF_Height = (PDF_Width*1.5)+(top_left_margin*2);
+    var canvas_image_width = HTML_Width;
+    var canvas_image_height = HTML_Height;
+    
+    var totalPDFPages = Math.ceil(HTML_Height/PDF_Height)-1;
+    
+
+    html2canvas($(".printThis")[0],{allowTaint:true}).then(function(canvas) {
+        canvas.getContext('2d');
+        
+        console.log(canvas.height+"  "+canvas.width);
+        
+        
+        var imgData = canvas.toDataURL("image/jpeg", 1.0);
+        var pdf = new jsPDF('p', 'pt',  [PDF_Width, PDF_Height]);
+        pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin,canvas_image_width,canvas_image_height);
+        
+        
+        // for (var i = 1; i <= totalPDFPages; i++) { 
+        //     pdf.addPage([PDF_Width, PDF_Height]);
+        //     pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height*i)+(top_left_margin*4),canvas_image_width,canvas_image_height);
+        // }
+
+        pdf.addPage([PDF_Width, PDF_Height]);
+        pdf.text("yes", 500, 50);
+        
+        pdf.save("HTML-Document.pdf");
+    });
+};
